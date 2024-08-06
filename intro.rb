fddea90328f61e444689ca150d217ed832797494
Toms Mediņš <43333020+MedinsT@users.noreply.github.com>
2024-08-06 05:32:56 +0000

    First ruby code

---
+++ intro.rb
@@ -0,0 +1,14 @@
+puts 'enter first value:'
+x1 = gets.to_i
+
+puts 'enter second value:'
+x2 = gets.to_i
+
+puts "X1: #{x1}, X2: #{x2}"
+
+begin
+  puts "devision result: #{x1 / x2}"
+rescue StandardError
+  puts 'PLEASE DO NOT DEVIDE BY ZERO!!!'
+end
+
